e5deba393ca75c3dd31c5dfb1f6d8793c6720edf
Alex Baranov <alexb@moveo.co.il>
2022-04-28 17:12:57 +0300

    adding http to url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'ec2-18-184-222-49.eu-central-1.compute.amazonaws.com/api/v1',
+  url: 'http://ec2-18-184-222-49.eu-central-1.compute.amazonaws.com/api/v1',
 };
